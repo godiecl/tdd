e1d95ec5032a5751622399b1abe6fb39ba50819d
Diego Urrutia Astorga <mika.cl@gmail.com>
2020-05-13 21:51:12 -0400

    feat(Entity): add utility class to help the model

---
+++ src/main/java/cl/ucn/disc/pdbp/utils/Entity.java
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Diego Urrutia-Astorga <durrutia@ucn.cl>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package cl.ucn.disc.pdbp.utils;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * The utils of Entity.
+ *
+ * @author Diego Urrutia-Astorga.
+ */
+public final class Entity {
+
+    /**
+     * Transforma en String un objeto t mostrando sus atributos.
+     *
+     * @param t   to convert.
+     * @param <T> type of t.
+     * @return the object in string format.
+     */
+    public static <T> String toString(final T t) {
+        return ReflectionToStringBuilder.toString(t, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+}
